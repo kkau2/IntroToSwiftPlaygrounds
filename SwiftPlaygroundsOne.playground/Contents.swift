37b35cf96da0bbd3a8e73c1e804b6168c5e3b07d
kkau2 <46541270+kkau2@users.noreply.github.com>
2019-01-17 10:47:22 -1000

    Complete Swift Playground

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -6,9 +6,9 @@ var str = "Hello, playground"
 
 //Swift Playground #1
 //Below we declare and set a variable to data type String
-var myFirstVariable: String
+//var myFirstVariable: String
 //Now we assign variable to a string
-myFirstVariable = "I've created my first variable"
+var myFirstVariable = "I've created my first variable"
 //Declare and assign all in one line
 var mySecondVariable = "and look I added two strings together"
 //Let's add to strings together
